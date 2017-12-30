3192d6b2162ce86e141b64e2bce089050252306e
8_n3z <jesus.saavedra37926637@gmail.com>
2017-12-29 17:15:57 -0700

    implemented file stack doing nothing atm, also using chuy's api key

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import RegisterLoginModal from "./RegisterLoginModal";
+import ReactFilestack from "filestack-react";
 
 class Header extends Component {
   render() {
@@ -8,6 +9,13 @@ class Header extends Component {
       <header className="Header">
         <text>Welcome</text>
         <RegisterLoginModal />
+        <ReactFilestack
+          apikey={"Av2OyyRf4Q16K5npkOJpBz"}
+          buttonText="FileStack Open"
+          buttonClass="FileStack"
+          // options={options}
+          onSuccess={this.yourCallbackFunction}
+        />
       </header>
     );
   }
